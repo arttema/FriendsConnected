6f4768891f33cad028932b8f0ae0aee07f11b7dd
Artem Stolpovski <ArtemStolpovski@coherentsolutions.com>
2014-09-08 20:06:07 +0300

    Init commit

---
+++ src/com/stalpouski/Connection.java
@@ -0,0 +1,12 @@
+package com.stalpouski;
+
+/**
+ * Healthland © FriendsConnected 2014
+ * All rights reserved
+ * Created by Artem Stolpovski on 08.09.2014.
+ */
+public class Connection {
+    public int friend2Id;
+    public int friend1Id;
+    int timestamp;
+}
---
+++ src/com/stalpouski/Main.java
@@ -0,0 +1,35 @@
+package com.stalpouski;
+
+/**
+ * Social network connectivity. Given a social network containing N members
+ * and a log file containing M timestamps at which times pairs of members formed
+ * friendships, design an algorithm to determine the earliest time at which all
+ * members are connected (i.e., every member is a friend of a friend of a friend
+ * ... of a friend). Assume that the log file is sorted by timestamp and that
+ * friendship is an equivalence relation. The running time of your algorithm
+ * should be MlogN or better and use extra space proportional to N.
+ */
+public class Main {
+    int[] friends = new int[100];
+    Connection[] connections = new Connection[1000];
+    BinaryDu connections
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+    public void whenEverybodyFriend(){
+        int reqConnections = friends.length;
+        int relationshipCount = 0;
+
+        for (Connection connection : connections) {
+            int first = connection.friend1Id;
+            int second = connection.friend2Id;
+
+            if[id]
+        }
+
+    }
+}
